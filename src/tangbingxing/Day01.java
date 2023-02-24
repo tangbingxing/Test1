1ebc2f31aed1525a4a1e901e293d6b8175313da7
unknown <tangbingxingtpx@gmail.com>
2023-02-24 14:26:54 +0800

    上传了123.txt

---
+++ src/tangbingxing/Day01.java
@@ -0,0 +1,9 @@
+package tangbingxing;
+
+public class Day01 {
+    public static void main(String[] args) {
+        int a = 10,b = 20;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ src/tangbingxing/ForDemo.java
@@ -0,0 +1,9 @@
+package tangbingxing;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
